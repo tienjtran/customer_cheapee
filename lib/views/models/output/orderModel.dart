a023e03de510d532db59ba8329857fbe0ab43e8f
Thien Vu <thienvx@outlook.com>
2021-03-07 09:50:40 +0700

    [Mobile][C6][CA-35] Edit order UI

--- lib/views/models/output/orderModel.dart
+++ lib/views/models/output/orderModel.dart
@@ -1,10 +1,15 @@
-class OrderModel{
+import 'package:customer_cheapee/views/models/output/product.dart';
+
+class OrderModel {
+  int id;
   String name;
   int quantity;
   double total;
   String imagePath;
+  ProductModel product;
 
-  OrderModel(this.name, this.quantity, this.total, this.imagePath);
+  OrderModel(
+      this.name, this.quantity, this.total, this.imagePath, this.product);
 
   get getName => this.name;
   set setName(name) => this.name = name;
@@ -14,5 +19,6 @@ class OrderModel{
   set setTotal(total) => this.total = total;
   get getImagePath => this.imagePath;
   set setImagePath(imagePath) => this.imagePath = imagePath;
+  get getProduct => this.product;
+  set setProduct(product) => this.product = product;
 }
-
